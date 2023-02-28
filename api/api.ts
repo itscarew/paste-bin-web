f71bb9d4e0ff23f5bfdc6a7fd0e91a45a3f6e67b
itscarew <itscarew@gmail.com>
2023-02-28 16:37:59 +0100

    new changes to url

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const PasteApi = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://olas-paste-bin.onrender.com/",
   headers: {
     "content-type": "application/json",
   },
